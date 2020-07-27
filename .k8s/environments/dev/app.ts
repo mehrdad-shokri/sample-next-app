5d5fce472ef3a5cae312cba87bd58bd23f28ed4a
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2020-07-27 13:54:23 +0200

    tmp

---
+++ .k8s/environments/dev/app.ts
@@ -0,0 +1,28 @@
+import { AppComponentEnvironment } from "@socialgouv/kosko-charts/components/app/params";
+
+export default {
+  name: "app",
+  image: {
+    name: process.env.CI_REGISTRY_IMAGE,
+    tag: process.env.CI_COMMIT_TAG
+      ? process.env.CI_COMMIT_TAG.slice(1)
+      : process.env.CI_COMMIT_SHA,
+  },
+  ingress: {
+    secretName: process.env.PRODUCTION ? "www-crt" : "wildcard-crt",
+  },
+  labels: {
+    component: "next",
+  },
+  requests: {
+    cpu: "1m",
+    memory: "64Mi",
+  },
+  limits: {
+    cpu: "50m",
+    memory: "128Mi",
+  },
+  containerPort: 3000,
+  servicePort: 3000,
+  namespace: { name: "xxx" },
+} as Readonly<AppComponentEnvironment>;
